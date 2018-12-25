5f52a2c064d61be206ef2e9fdddce756a5da1b6e
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-12-25 18:25:27 +0200

    First workeable version

--- src/EnumeratedList.elm
+++ src/EnumeratedList.elm
@@ -1,10 +1,20 @@
 module EnumeratedList exposing
-    ( EnumeratedList(..)
+    ( EnumeratedItem
+    , EnumeratedList(..)
     , fromList
+    , length
     , toList
+    , updateAtItem
+    , map
     )
 
 import List
+
+
+type alias EnumeratedItem a =
+    { position : Int
+    , element : a
+    }
 
 
 type EnumeratedList a
@@ -18,8 +28,23 @@ fromList =
 
 toList : EnumeratedList a -> List a
 toList (EnumeratedList xs) =
-    List.map snd xs
+    List.map Tuple.second xs
 
 
-snd ( a, b ) =
-    b
+map : (EnumeratedItem a -> b) -> EnumeratedList a -> EnumeratedList b
+map =
+    Debug.todo "todo"
+
+
+updateAtItem :
+    EnumeratedItem a
+    -> (EnumeratedItem a -> Maybe a)
+    -> EnumeratedList a
+    -> EnumeratedList a
+updateAtItem =
+    Debug.todo "todo"
+
+
+length : EnumeratedList a -> Int
+length =
+    List.length << toList
